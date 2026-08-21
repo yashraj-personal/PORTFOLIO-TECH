7f1b3730163c80f66d5984a7bfc7fb5c9cc5e682
YASH RAJ <akhisweety01@gmail.com>
2026-08-21 18:55:47 +0530

    Add files via upload

---
+++ TECH-PORTFOLIO/app/robots.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://yashraj.dev'
+
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/', '/admin/'],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
+}
